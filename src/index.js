0f2019bc9c50805bef0adc5bc92fc29e398e6ee6
MithunGowda <mithungowdahc12a@gmail.com>
2024-10-25 18:49:41 +0530

    cors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 // require('dotenv').config()
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
 import connectDB from "./db/index.js";
-
+import app from "./app.js";
 
 dotenv.config({
-  path: './env'
-})
-connectDB()
+  path: "./env",
+});
+connectDB().then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`App is listening at port ${process.env.PORT}`);
+  });
+}).catch((error)=>{
+  console.log("Error")
+
+});
 /*
 import express from "express"
 
