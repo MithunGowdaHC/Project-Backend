6091a9e013f83540430965c67ba47e28d3eb2962
MithunGowda <mithungowdahc12a@gmail.com>
2024-10-31 09:28:28 +0530

    jwt

--- src/app.js
+++ src/app.js
@@ -5,12 +5,18 @@ import cors from "cors"
 const app = express()
 
 app.use(cors({
-    origin:process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN,
     Credentials: true
-    
+
 }))
 app.use(express.json({
-limit:"16kb"
+    limit: "16kb"
 }))
-app.use(express.urlencoded({extended}))
+app.use(express.urlencoded({
+    extended: true, limit: "16kb"
+
+}))
+
+app.use(express.static("public"))
+app.use(cookieParser())
 export default app;
