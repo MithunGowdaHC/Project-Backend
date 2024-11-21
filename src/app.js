ef84a263dc1bf5883f3346fb34ad618b45b92d30
MithunGowda <mithungowdahc12a@gmail.com>
2024-11-21 20:24:52 +0530

    two

--- src/app.js
+++ src/app.js
@@ -1,25 +1,30 @@
-import express from "express"
+import express from "express";
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    Credentials: true
+    Credentials: true,
+  })
+);
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "16kb",
+  })
+);
 
-}))
-app.use(express.json({
-    limit: "16kb"
-}))
-app.use(express.urlencoded({
-    extended: true, limit: "16kb"
-
-}))
-
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.static("public"));
+app.use(cookieParser());
 
 import userRouter from "./routes/user.routes.js";
-app.use("/api/v1/users", userRouter)
+app.use("/api/v1/users", userRouter);
 export default app;
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ dotenv.config({
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-      console.log(`App is listening at port ${process.env.PORT || 8000}  `);
+      console.log(`App is listening at port ${process.env.PORT || 8000}`);
     });
   })
   .catch((error) => {
